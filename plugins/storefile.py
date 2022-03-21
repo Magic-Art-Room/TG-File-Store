d5039daca9219c2ec1c805b13d36422bf4ebd7d4
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-03-21 23:50:16 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -48,7 +48,7 @@ async def storefile(c, m):
     bot = await c.get_me()
     url = f"https://t.me/ArkFileStoreBot?start={m.chat.id}_{m.message_id}" if not DB_CHANNEL_ID else f"https://t.me/ArkFileStoreBot?start={m.chat.id}_{msg.message_id}"
     txt = urllib.parse.quote(text.replace('--', ''))
-    share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
+    share_url = f"tg://share?url=File%20Link%20👉%20{url}"
 
     # making buttons
     buttons = [[
